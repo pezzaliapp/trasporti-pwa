0935eebaa31f8a923ebba436b22a69decc94e965
pezzaliapp <pezzalialessandro@gmail.com>
2026-01-27 16:39:39 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,37 @@
-const CACHE = "trasporti-pwa-v1";
+const CACHE = "trasporti-pwa-v2"; // bump version when assets change
+
 const ASSETS = [
   "./",
   "./index.html",
   "./styles.css",
   "./app.js",
   "./manifest.json",
+
+  // Data
   "./data/articles.json",
   "./data/pallet_rates_by_region.json",
   "./data/groupage_rates.json",
+  "./data/geo_provinces.json",
+  "./data/geo_provinces.csv",
+  "./data/template_articles.csv",
+
+  // Icons
   "./icons/icon-192.png",
   "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null)))
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)))
+    )
   );
   self.clients.claim();
 });
